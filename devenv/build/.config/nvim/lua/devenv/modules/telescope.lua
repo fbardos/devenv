b99a56f59ae4d3cd3606834876bd5d6422dd9f1f
Fabian Bardos <fabian.bardos@gmail.com>
2023-12-22 13:17:37 +0100

    Show hidden files in telescope
    Added custom Lua function to show hidden files and untracked git files
    when using telescope find_files.

--- devenv/build/.config/nvim/lua/devenv/modules/telescope.lua
+++ devenv/build/.config/nvim/lua/devenv/modules/telescope.lua
@@ -1,3 +1,12 @@
+local function find_files()
+    local opts = {
+        theme="ivy",
+        hidden = true,
+        show_untracked = true,
+    }
+    require("telescope.builtin").find_files(opts)
+end
+
 return {
     "nvim-telescope/telescope.nvim",
     dependencies = {
@@ -26,7 +35,7 @@ return {
         -- set keymaps
         local keymap = vim.keymap -- for conciseness
 
-        keymap.set("n", "<leader>f", "<cmd>Telescope find_files theme=ivy<cr>", { desc = "Fuzzy find files in cwd" })
+        keymap.set("n", "<leader>f", find_files, { desc = "Fuzzy find files in cwd" })
         keymap.set("n", "<leader>F", "<cmd>Telescope live_grep theme=ivy<cr>", { desc = "Find string in cwd" })
         keymap.set("n", "<leader>g", "<cmd>lua require('telescope.builtin').lsp_document_symbols(require('telescope.themes').get_ivy({show_line = true}))<cr>", { desc = "Find LSP symbols in current file" })
         keymap.set("n", "<leader>t", "<cmd>Telescope treesitter theme=ivy<cr>", { desc = "Fuzzy find in treesitter" })
