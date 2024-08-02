392299f53e9e87a42078ffbdfb8878c92f2d6fd0
Fabian Bardos <fabian.bardos@gmail.com>
2024-08-02 12:02:28 +0200

    FIX <Tab> conflict, moved copilot cmp to <C-\>

--- devenv/build/.config/nvim/lua/devenv/modules/copilot.lua
+++ devenv/build/.config/nvim/lua/devenv/modules/copilot.lua
@@ -1,3 +1,11 @@
 return {
-    "github/copilot.vim"
+    "github/copilot.vim",
+    config = function()
+
+        -- set keymaps
+        -- source https://github.com/orgs/community/discussions/8105
+        local keymap = vim.keymap
+        vim.cmd('imap <silent><script><expr> <C-\\> copilot#Accept("")')
+        vim.cmd("let g:copilot_no_tab_map = v:true")
+    end,
 }
