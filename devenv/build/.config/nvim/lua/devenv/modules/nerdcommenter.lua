943a161d266134379e73bc19de6d7a0cb4870179
Fabian Bardos <fabian@bardos.dev>
2025-02-23 12:40:15 +0000

    Changed way of nerdcommenting
    Instead of commenting in line, respecting the intendation, just do a
    aligned comment block. This is better readable and works better with
    black (python).

--- devenv/build/.config/nvim/lua/devenv/modules/nerdcommenter.lua
+++ devenv/build/.config/nvim/lua/devenv/modules/nerdcommenter.lua
@@ -1,3 +1,6 @@
 return {
     "scrooloose/nerdcommenter",  -- comment/uncomment
+    config = function()
+      vim.g.NERDDefaultAlign = "left"  -- works better with black (python) and is better visible
+    end,
 }
