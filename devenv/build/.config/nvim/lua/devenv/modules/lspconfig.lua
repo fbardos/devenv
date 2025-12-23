1b63f0d14adf0bd87936afed234d5d8c159cadf3
Fabian Bardos <fabian@bardos.dev>
2025-12-23 16:49:01 +0000

    Migrated to nvim-treesitter main
    A complete rewrite of the nvim-treesitter is now available (branch: main
    instead of master). Adopted configuration accordingly:
    
     - No `ensure_installed` on nvim-treesitter.lua anymore. The
       installation of the desired languages happen directly in Dockerfile
       with TSInstall.
     - Modified init.lua, to map autocmd to Filetypes and start treesitter
     - Force to install treesitter languages synchronously in the
       Dockerfile.

--- devenv/build/.config/nvim/lua/devenv/modules/lspconfig.lua
+++ devenv/build/.config/nvim/lua/devenv/modules/lspconfig.lua
@@ -39,26 +39,9 @@ return {
             opts.desc = "See available code actions"
             keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
 
-            -- Highlighting references.
-            -- See: https://sbulav.github.io/til/til-neovim-highlight-references/
-            -- for the highlight trigger time see: `vim.opt.updatetime`
             vim.opt.updatetime = 300
-            if client.server_capabilities.documentHighlightProvider then
-                vim.api.nvim_create_augroup("lsp_document_highlight", { clear = true })
-                vim.api.nvim_clear_autocmds { buffer = bufnr, group = "lsp_document_highlight" }
-                vim.api.nvim_create_autocmd("CursorHold", {
-                    callback = vim.lsp.buf.document_highlight,
-                    buffer = bufnr,
-                    group = "lsp_document_highlight",
-                    desc = "Document Highlight",
-                })
-                vim.api.nvim_create_autocmd("CursorMoved", {
-                    callback = vim.lsp.buf.clear_references,
-                    buffer = bufnr,
-                    group = "lsp_document_highlight",
-                    desc = "Clear All the References",
-                })
-            end
+            -- No highlighting references anymore, will be handled by Treesitter directly
+	    -- (using the colors from colorscheme)
         end
 
         -- used to enable autocompletion (assign to every lsp server config)
--- devenv/build/.config/nvim/lua/devenv/modules/nvim-treesitter.lua
+++ devenv/build/.config/nvim/lua/devenv/modules/nvim-treesitter.lua
@@ -5,28 +5,7 @@ return {
     dependencies = {
       "windwp/nvim-ts-autotag",
     },
-    config = function()
-        require("nvim-treesitter.config").setup({
-            highlight = { enable = true },
-            indent = { enable = true },
-            autotag = { enable = true },
-            ensure_installed = {
-                "json",
-                "yaml",
-                "html",
-                "css",
-                "markdown",
-                "markdown_inline",
-                "bash",
-                "lua",
-                "vim",
-                "dockerfile",
-                "gitignore",
-                "python",
-                "sql",
-            },
-        })
-
-    end,
-
+    -- with the rewrite of nvim-treesitter, ensure_installed
+    -- has no effect anymore. installation of the languages
+    -- happen directly in the Dockerfile
 }
